2f178d47dd04707596719a3cc6bd4db04205d8d0
CodeWithMohaimin <dev.mohaimin@gmail.com>
2024-05-30 16:29:08 +0600

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import GoogleAnalytics from '../components/GoogleAnalytics';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
